1b45adbe68cf03e42a4252c5502adbac04d022b3
PedroMarquetti <pedro.marquettti@gmail.com>
2022-04-25 20:08:02 -0300

    updated setup script

--- .local/bin/setup.sh
+++ .local/bin/setup.sh
@@ -40,7 +40,7 @@ setup_git(){
 }
 
 install_ohmy(){
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
     print_cyan "installing zsh-autosuggestions"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
