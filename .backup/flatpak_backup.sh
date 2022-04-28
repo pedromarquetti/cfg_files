10b03e8bcae7f3595771aefb233f5ecb7c9a6b5f
PedroMarquetti <pedro.marquettti@gmail.com>
2022-04-28 15:16:43 -0300

    added a script in .backup to install some flatpak apps

---
+++ .backup/flatpak_backup.sh
@@ -0,0 +1,5 @@
+flatpak install  --user "com.bitwarden.desktop" 
+flatpak install  --user "com.discordapp.Discord" 
+flatpak install  --user "com.spotify.Client" 
+flatpak install  --user "org.onlyoffice.desktopeditors" 
+flatpak install  --user "org.telegram.desktop" 
